96b5e99369f217d9e672d6bc1abdac102997be2f
David Beath <davidgbeath@gmail.com>
2017-12-07 00:01:56 +0100

    Add search form

--- gateway/application.py
+++ gateway/application.py
@@ -1,22 +1,41 @@
 from flask import Flask, request, jsonify, render_template
 from feedsearch import search
 from .feedinfo_schema import FEED_INFO_SCHEMA
+import json
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        url = request.form.get('url')
+
+        feed_list = search(url, info=True)
+
+        return render_template('results.html', feeds=feed_list)
+
     return render_template('index.html')
 
 @app.route('/search', methods=['GET'])
-def run_search():
+def search_api():
     url = request.args.get('url')
 
     feed_list = search(url, info=True)
+
+    print(feed_list)
+
+    # if request.args.get('result'):
+    #     return render_template('results.html', feeds=feed_list)
 
     result, errors = FEED_INFO_SCHEMA.dump(feed_list)
     if errors:
         app.logger.warning('Dump errors: {0}'.format(errors))
         return '', 500
 
+    if request.args.get('result'):
+        return render_template('results.html', feeds=get_pretty_print(result), url=url)
+
     return jsonify(result)
+
+def get_pretty_print(json_object):
+    return json.dumps(json_object, sort_keys=True, indent=2, separators=(',', ': '))
