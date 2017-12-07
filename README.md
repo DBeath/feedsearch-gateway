c44f8532bd0342df620894a66320ec15679f5a96
David Beath <davidgbeath@gmail.com>
2017-12-07 18:38:30 +0100

    Add certficate, use S3 for static assets

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # feedsearch-gateway
-Serverless AWS API using Zappa for Feedsearch
+Serverless AWS API using [Zappa](https://github.com/Miserlou/Zappa) for the [Feedsearch Python package](https://github.com/DBeath/feedsearch).
+
+Live at [Feedsearch](https://feedsearch.auctorial.com/)
--- gateway/application.py
+++ gateway/application.py
@@ -1,9 +1,12 @@
 from flask import Flask, request, jsonify, render_template
+from flask_s3 import FlaskS3
 from feedsearch import search
 from .feedinfo_schema import FEED_INFO_SCHEMA
 import json
 
 app = Flask(__name__)
+app.config['FLASKS3_BUCKET_NAME'] = 'zappa-mrxw2pac1'
+s3 = FlaskS3(app)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -21,11 +24,6 @@ def search_api():
     url = request.args.get('url')
 
     feed_list = search(url, info=True)
-
-    print(feed_list)
-
-    # if request.args.get('result'):
-    #     return render_template('results.html', feeds=feed_list)
 
     result, errors = FEED_INFO_SCHEMA.dump(feed_list)
     if errors:
