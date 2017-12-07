a61d84d5f0d12b54d8a29df7c09fefa5a4574028
David Beath <davidgbeath@gmail.com>
2017-12-08 00:27:12 +0100

    Add search time to result

--- gateway/application.py
+++ gateway/application.py
@@ -3,6 +3,7 @@ from flask_s3 import FlaskS3
 from feedsearch import search
 from .feedinfo_schema import FEED_INFO_SCHEMA
 import json
+import time
 
 app = Flask(__name__)
 app.config['FLASKS3_BUCKET_NAME'] = 'zappa-mrxw2pac1'
@@ -23,9 +24,14 @@ def index():
 def search_api():
     url = request.args.get('url')
 
+    start_time = time.perf_counter()
+
     feed_list = search(url, info=True)
 
     result, errors = FEED_INFO_SCHEMA.dump(feed_list)
+
+    search_time = int((time.perf_counter() - start_time) * 1000)
+
     if errors:
         app.logger.warning('Dump errors: {0}'.format(errors))
         return '', 500
@@ -33,7 +39,7 @@ def search_api():
     if request.args.get('result'):
         return render_template('results.html', feeds=get_pretty_print(result), url=url)
 
-    return jsonify(result)
+    return jsonify({'feeds': result, 'search_time_ms': search_time})
 
 def get_pretty_print(json_object):
     return json.dumps(json_object, sort_keys=True, indent=2, separators=(',', ': '))
